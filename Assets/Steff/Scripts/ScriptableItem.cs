73c9daffaf7e58660afe55092d0b389b4429d568
Stavrin <steffanleprince@gmail.com>
2023-01-08 14:48:28 +0000

    Shop functionality and UI almost finished.

--- Assets/Steff/Scripts/ScriptableItem.cs
+++ Assets/Steff/Scripts/ScriptableItem.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ShopItem", menuName = "Custom Data/ShopItem")]
 public class ScriptableItem : ScriptableObject
 {
-    [SerializeField] private int itemID; //might not be needed because the list will have index numbers
+    [SerializeField] private string itemID; //might not be needed because the list will have index numbers
 
-    [SerializeField] private string itemName;
-    [SerializeField] private string itemDescription;
-    [SerializeField] private string itemPrice;
-    [SerializeField] private Sprite itemImage;
+    public string itemName;
+    public string itemDescription;
+    public string itemPrice;
+    public Sprite itemImage;
     
-    public int ItemID { get { return itemID; } }
+    public string ItemID { get { return itemID; } }
 
     public void SetItemID (int id)
     {
--- Assets/Steff/Scripts/ShopManager.cs
+++ Assets/Steff/Scripts/ShopManager.cs
@@ -7,10 +7,25 @@ public class ShopManager : MonoBehaviour
 {
     [SerializeField] List<ScriptableItem> items;
     [SerializeField] TMPro.TextMeshProUGUI[] itemName, itemDescription, itemPrice;
+    [SerializeField] private Image[] itemImage;
+    [SerializeField] private TMPro.TextMeshProUGUI wallet;
+    
+    [SerializeField] ScriptableItem playerWallet;
+    
     
     // Start is called before the first frame update
     void Start()
     {
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            itemName[i].text = items[i].itemName;
+            itemDescription[i].text = items[i].itemDescription;
+            itemPrice[i].text = items[i].itemPrice;
+            itemImage[i].sprite = items[i].itemImage;
+
+            wallet.text = playerWallet.ItemID;
+        }
 
 
         
